d5328ee2968cde40194ddc33abfcffe2be42e24d
shadow-ryu <vishnukulkarni4302@gmail.com>
2025-05-12 02:20:15 +0530

    googlesearch mapping

---
+++ next-sitemap.config.js
@@ -0,0 +1,9 @@
+/** next-sitemap.config.js */
+module.exports = {
+    siteUrl: 'https://www.buildsforge.com',
+    generateRobotsTxt: true,
+    changefreq: 'daily',
+    priority: 0.7,
+    sitemapSize: 5000,
+  };
+  
