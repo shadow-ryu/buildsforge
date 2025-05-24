ba584b3b7b1808f4962de6ba8bedf445c3e69dc8
shadow-ryu <vishnukulkarni4302@gmail.com>
2025-05-24 22:33:07 +0530

    done

--- components/header.tsx
+++ components/header.tsx
@@ -1,7 +1,5 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Bell, Search } from "lucide-react";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import { SidebarTrigger } from "./ui/sidebar";
 
@@ -11,25 +9,25 @@ export default function Header() {
       <div className="flex items-center gap-3">
         <SidebarTrigger />
         <div className="hidden md:flex items-center relative max-w-md">
-          <div className="absolute left-3 text-gray-400">
+          {/* <div className="absolute left-3 text-gray-400">
             <Search className="h-4 w-4" />
           </div>
           <input 
             type="text" 
             placeholder="Search..." 
             className="pl-10 pr-4 py-2 bg-[#181A20] border border-purple-900/30 rounded-md text-sm text-white w-full focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent"
-          />
+          /> */}
         </div>
       </div>
 
       <div className="flex items-center gap-4">
-        <Button
+        {/* <Button
           variant="ghost"
           size="icon"
           className="rounded-full hover:bg-purple-950/30 transition"
         >
           <Bell className="w-5 h-5 text-gray-400 hover:text-purple-400 transition" />
-        </Button>
+        </Button> */}
         <SignedIn>
           <UserButton
             appearance={{
